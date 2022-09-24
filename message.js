1dbdb4e98c5c6796708fff6e3a9fc9ea2f019887
quinn <quinnjohnson24@gmail.com>
2022-09-24 01:11:22 -0500

    Final Commit

--- message.js
+++ message.js
@@ -1,7 +1,10 @@
-var message = document.getElementById("message");
 
-function createMessage(){
+function func(){
+    const phrases = ["You can do it", "Dont give up!", "What would Tom Cruise Do?"];
+    var randNum1 = Math.floor(Math.random() * phrases.length);
+    var randNum2 = Math.floor(Math.random() * phrases.length);
 
-    
+    var completePhrase = phrases[randNum1] + " " + phrases[randNum2];
 
+    document.getElementById('message').innerHTML = completePhrase;
 }
